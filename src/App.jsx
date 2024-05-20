884a46900a7fd02bcdfab4dbfa32e721050fc422
taeyun <lwe12f675@gmail.com>
2024-05-20 18:13:27 +0900

    ex4 과제 완료

--- src/App.jsx
+++ src/App.jsx
@@ -10,20 +10,23 @@ function App() {
     { name: "Diana", age: 19, grade: "D" },
   ];
 
-  const [filteredStudents, setFilteredStudents] = useState(initialStudents);
+  const [filteredStudents, setFilteredStudents] =
+    useState(initialStudents);
 
   return (
     <div>
       <h1>학생 목록</h1>
       {/* TODO: FilterButtons 컴포넌트를 작성하고 필요한 props를 전달하세요. */}
       <FilterButtons
-      /* 필요한 props를 여기에 전달하세요. */
+        initialStudents={initialStudents}
+        setFilteredStudents={setFilteredStudents}
+        /* 필요한 props를 여기에 전달하세요. */
       />
-
       {/* TODO: StudentList 컴포넌트를 작성하고 필요한 props를 전달하세요. */}
-      <StudentList
-      /* 필요한 props를 여기에 전달하세요. */
-      />
+      {filteredStudents.map((student) => (
+        <StudentList key={student.name} {...student} />
+      ))}
+      {/* 필요한 props를 여기에 전달하세요. */}
     </div>
   );
 }
